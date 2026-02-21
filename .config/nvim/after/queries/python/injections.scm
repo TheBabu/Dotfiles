61e97c21bf9c51786a7995a1e14a1a14b70db605
TheBabu <ariq.haqq@gmail.com>
2026-02-20 21:04:52 -0700

    Update lots of dotfiles

---
+++ .config/nvim/after/queries/python/injections.scm
@@ -0,0 +1,6 @@
+;; extends
+
+((comment) @injection.content
+ (#offset! @injection.content 0 1 0 0)
+ (#set! injection.language "comment"))
+
--- .config/nvim/lua/configs/lspconfig.lua
+++ .config/nvim/lua/configs/lspconfig.lua
@@ -1,24 +1,18 @@
-local on_attach = require("nvchad.configs.lspconfig").on_attach
-local on_init = require("nvchad.configs.lspconfig").on_init
-local capabilities = require("nvchad.configs.lspconfig").capabilities
+require("nvchad.configs.lspconfig").defaults()
 
-local lspconfig = require("lspconfig")
 local servers = { "html", "cssls", "clangd", "pylsp", "rust_analyzer" }
 
-for _, lsp in ipairs(servers) do
-    lspconfig[lsp].setup({
-        on_attach = on_attach,
-        on_init = on_init,
-        capabilities = capabilities,
-        settings = lsp == "pylsp" and {
-            pylsp = {
-                plugins = {
-                    pycodestyle = {
-                        enabled = false
-                    }
+vim.lsp.config("pylsp", {
+    settings = {
+        pylsp = {
+            plugins = {
+                pycodestyle = {
+                    enabled = false
                 }
             }
-        } or {}
-    })
-end
+        }
+    }
+})
+
+vim.lsp.enable(servers)
 
